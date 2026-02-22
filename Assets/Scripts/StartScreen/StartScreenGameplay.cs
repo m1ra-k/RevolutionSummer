6fdb497d8e7d3d0ccd4cea5eaffecfddefd77737
mocha <mirakhan.mk910@gmail.com>
2026-02-22 08:05:21 -0500

    credits on start screen

--- Assets/Scripts/StartScreen/StartScreenGameplay.cs
+++ Assets/Scripts/StartScreen/StartScreenGameplay.cs
@@ -2,6 +2,23 @@ using UnityEngine;
 
 public class StartScreenGameplay : MonoBehaviour
 {
+    private GameObject buttons;
+    private GameObject credits;
+
+    void Awake()
+    {
+        buttons = GameObject.Find("Canvas").transform.Find("Buttons").gameObject;
+
+        credits = GameObject.Find("Canvas").transform.Find("Credits").gameObject;
+        credits.SetActive(false);
+    }
+
+    public void ToggleCredits()
+    {
+        credits.SetActive(!credits.activeSelf);
+        buttons.SetActive(!credits.activeSelf);
+    }
+
     public void ExitGame()
     {
         Application.Quit();
